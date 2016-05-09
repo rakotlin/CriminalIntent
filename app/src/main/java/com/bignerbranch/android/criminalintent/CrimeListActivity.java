5f34f52c0b8df54f49bf5c327e857e626292b8c7
RaKotlin <rakotlin@gmail.com>
2016-05-09 17:53:38 +0300

    Abstract

--- app/src/main/java/com/bignerbranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerbranch/android/criminalintent/CrimeListActivity.java
@@ -2,7 +2,10 @@ package com.bignerbranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by User on 09.05.2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment(){
         return new CrimeListFragment();
---
+++ app/src/main/java/com/bignerbranch/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package com.bignerbranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by User on 09.05.2016.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
+
+    }
+
+}
